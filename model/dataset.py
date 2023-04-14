959e7683e1bbc8617cc8fd9033176e6e0f7b5631
dancingtree <manicgroans@mail.ru>
2023-04-14 21:36:24 +0300

    adding MLP_Pick_scorer model and add increment of card ids in batch due to reserved padding idx 0

--- model/dataset.py
+++ model/dataset.py
@@ -1,6 +1,33 @@
 from torch.utils.data import Dataset, DataLoader
+from torch import LongTensor
+from torch.nn.utils.rnn import pad_sequence
 import pandas as pd
-from model.utils import save_on_disc, load_from_disc
+from model.utils import load_from_disc
+from tqdm import tqdm
+
+
+def custom_collate_fn(batch):
+    batch = list(zip(*batch))
+    if len(batch) == 2:
+        pool, cand = batch
+
+        pool = [LongTensor(row) + 1 for row in pool]
+        pool = pad_sequence(pool, batch_first = True)
+        
+        cand = LongTensor(cand) + 1
+
+        return pool, cand
+    
+    else:
+        meta, pool, cand = batch
+
+        pool = [LongTensor(row) + 1 for row in pool]
+        pool = pad_sequence(pool, batch_first = True)
+
+        cand = [LongTensor(row) + 1 for row in cand]
+        cand = pad_sequence(cand, batch_first = True)
+
+        return meta, pool, cand
 
 
 class DraftDataset(Dataset):
@@ -28,3 +55,16 @@ class DraftDataset(Dataset):
             return self.pool[idx], self.candidates[idx]
         else:
             return (self.draft_id[idx], self.pack_num[idx], self.pick_num[idx]), self.pool[idx], self.candidates[idx]
+
+
+
+if __name__ == '__main__':
+    dataset = DraftDataset('C:\\Users\\manic\\Desktop\\Draft.ai\\intermidiate data\\NEO_test', train = False)
+
+    dataloader = DataLoader(dataset, collate_fn=custom_collate_fn, batch_size= 100)
+
+    for meta, pool, cand in tqdm(dataloader):
+        print(meta)
+        print(pool)
+        print(cand)
+        break
---
+++ model/models.py
@@ -0,0 +1,77 @@
+import torch
+
+
+
+class MLP(torch.nn.Sequential):
+    
+    def __init__(
+                 self, input_size, hidden_sizes, 
+                 norm_layer = False, 
+                 activation_layer = torch.nn.ReLU, 
+                 dropout = 0.0
+                 ):
+        
+        layers = []
+        
+        in_dim = input_size
+        for hid_dim in hidden_sizes[:-1]:
+            layer = torch.nn.Linear(in_dim, hid_dim)
+            layers.append(layer)
+
+            if norm_layer is True:
+                layers.append(torch.nn.BatchNorm1d(hid_dim))
+
+            layers.append(activation_layer())
+
+            layers.append(torch.nn.Dropout(dropout))
+
+            in_dim = hid_dim
+
+        layer = torch.nn.Linear(in_dim, hidden_sizes[-1])
+        layers.append(layer)
+
+        super().__init__(*layers)
+
+
+class MLP_Pick_Scorer(torch.nn.Module):
+
+    def __init__(self, input_size, hidden_sizes, **kwargs):
+        super().__init__()
+
+        self.input_size = input_size
+
+        self._pool_encoder = MLP(input_size, hidden_sizes, **kwargs) 
+
+        self._cards_embedings = torch.nn.Embedding(input_size + 1, embedding_dim=hidden_sizes[-1], padding_idx=0)
+
+    def _pool_one_hot_encode(self, input):
+
+        return torch.nn.functional.one_hot(input, self.input_size + 1).sum(axis = 1)[:, 1:].to(torch.float32)
+    
+    def forward(self, pool, conds):
+        pool = self._pool_one_hot_encode(pool)
+        conds_embeds = self._cards_embedings(conds)
+        encoded_pool = self._pool_encoder(pool)
+        output = torch.bmm(conds_embeds, encoded_pool.unsqueeze(-1)).squeeze(-1)
+        return output
+
+            
+
+
+
+
+
+if __name__ == '__main__':
+    from model.dataset import DraftDataset, custom_collate_fn
+    from torch.utils.data import DataLoader
+
+    dataset = DraftDataset('C:\\Users\\manic\\Desktop\\Draft.ai\\intermidiate data\\NEO_test', train = False)
+
+    dataloader = DataLoader(dataset, collate_fn=custom_collate_fn, batch_size= 2, shuffle=False)
+
+    model = MLP_Pick_Scorer(287, [50])
+
+    for meta, pool, cond in dataloader:
+        output = model(pool, cond)
+        print(output)
+        break
