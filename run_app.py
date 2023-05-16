9b18d217d50b68f771ea3fca786f9cb047e407de
dancingtree <manicgroans@mail.ru>
2023-05-17 00:04:58 +0300

    some fixes

--- run_app.py
+++ run_app.py
@@ -1,19 +1,20 @@
 from mtg_draft_assistant.app.draft_assistent import CardManager, ArenaDraftAssistController
 from mtg_draft_assistant.app.arena_parser import ArenaParser
 from mtg_draft_assistant.app.gui import ArenaDraftAssistGUI
-from mtg_draft_assistant.app.models import PickRate
+from mtg_draft_assistant.app.models import PickRate, NeuralPickScorer
 
 
 MTG_LOG_PATH = "C:\\Users\\manic\\AppData\\LocalLow\\Wizards Of The Coast\\MTGA\\Player.log"
 #MTG_LOG_PATH = "C:\\Users\\manic\\Desktop\\Draft.ai\\test env\\testlog.txt"
 SET_DATA_FOLDER = "C:\\Users\\manic\\Desktop\\Draft.ai\\sets"
 INTR_MODEL_REPR_FOLDER = "C:\\Users\\manic\\Desktop\\Draft.ai\\modelIds"
-CARD_SCORES_PATH = "C:\\Users\\manic\\Desktop\\Draft.ai\\intermidiate data\\NEO_scores.json"
+CHEKPOINTS_PATH = "C:\\Users\\manic\\Desktop\\Draft.ai\\mtg_draft_assistant\\checkpoints"
+#CARD_SCORES_PATH = "C:\\Users\\manic\\Desktop\\Draft.ai\\intermidiate data\\NEO_scores.json"
 #CARD_SCORES_PATH = "C:\\Users\\manic\\Desktop\\Draft.ai\\intermidiate data\\cards_value.json"
 
 arena = ArenaParser(MTG_LOG_PATH)
 card_manager = CardManager(SET_DATA_FOLDER, INTR_MODEL_REPR_FOLDER)
-model = PickRate(CARD_SCORES_PATH)
+model = NeuralPickScorer(CHEKPOINTS_PATH)
 
 controller = ArenaDraftAssistController(arena, card_manager, model)
 
